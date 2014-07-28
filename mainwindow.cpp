b86298db69dd3690a339bb5a951cd527d4fc80ed
HanaeN <sinoc300@gmail.com>
2014-07-28 08:56:46 +0100

    Added signals/slots for editing keyframe properties. Keyframe property editing is done.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include "timelineheaderview.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -20,7 +22,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->e_Editor->setMouseTracking(true);
     headerView->resizeSection(0, 200);
     connect(delegate, SIGNAL(timelinePositionChange(int)), this, SLOT(onTimelinePositionChanged(int)));
-    connect(delegate, SIGNAL(keyframeSelected(KeyframeObject*)), this, SLOT(onKeyframeSelected(KeyframeObject*)));
+    connect(delegate, SIGNAL(keyframeSelected(KeyframeObject*,bool)), this, SLOT(onKeyframeSelected(KeyframeObject*,bool)));
 }
 
 
@@ -56,16 +58,120 @@ void MainWindow::onTimelinePositionChanged(int x) {
     ui->e_Editor->viewport()->update();
 }
 
-void MainWindow::onKeyframeSelected(KeyframeObject *obj) {
+void MainWindow::onKeyframeSelected(KeyframeObject *obj, bool isint) {
+    isInt = isint;
+    editingKeyframe = NULL;
     if (obj != NULL) {
         ui->e_value->setText(QString::number(obj->value));
         ui->e_easingin->setCurrentIndex(0);
         ui->grp_keyframe->setEnabled(true);
+        if (obj->easeIn != Keyframe::NoEasing) {
+            int id = 0;
+            if (obj->easeIn == Keyframe::CubicEasing) id = 1;
+            if (obj->easeIn == Keyframe::QuadraticEasing) id = 2;
+            if (obj->easeIn == Keyframe::SinusoidalEasing) id = 3;
+            if (obj->easeIn == Keyframe::ExponentialEasing) id = 4;
+            ui->c_easingin->setChecked(true);
+            ui->e_easingin->setCurrentIndex(id);
+            ui->e_easingin->setEnabled(true);
+        } else {
+            ui->e_easingin->setCurrentIndex(0);
+            ui->c_easingin->setChecked(false);
+            ui->e_easingin->setEnabled(false);
+        }
+
+        if (obj->easeOut != Keyframe::NoEasing) {
+            int id = 0;
+            if (obj->easeOut == Keyframe::CubicEasing) id = 1;
+            if (obj->easeOut == Keyframe::QuadraticEasing) id = 2;
+            if (obj->easeOut == Keyframe::SinusoidalEasing) id = 3;
+            if (obj->easeOut == Keyframe::ExponentialEasing) id = 4;
+            ui->c_easingout->setChecked(true);
+            ui->e_easingout->setCurrentIndex(id);
+            ui->e_easingout->setEnabled(true);
+        } else {
+            ui->e_easingout->setCurrentIndex(0);
+            ui->c_easingout->setChecked(false);
+            ui->e_easingout->setEnabled(false);
+        }
         editingKeyframe = obj;
     } else {
         ui->e_value->setText("0");
         ui->e_easingin->setCurrentIndex(0);
+        ui->e_easingout->setCurrentIndex(0);
+        ui->c_easingin->setChecked(false);
+        ui->c_easingout->setChecked(false);
         ui->grp_keyframe->setEnabled(false);
-        editingKeyframe = NULL;
+
     }
+}
+
+void MainWindow::on_e_value_textEdited(const QString &arg1)
+{
+    if (!arg1.endsWith(".")) {
+        double number = arg1.toDouble();
+        if (isInt) number = round(number);
+        editingKeyframe->value = number;
+        ui->e_value->setText(QString::number(number));
+    }
+}
+
+void MainWindow::on_e_value_editingFinished()
+{
+    if (ui->e_value->text().endsWith(".")) ui->e_value->setText(ui->e_value->text() + "0");
+    on_e_value_textEdited(ui->e_value->text());
+}
+
+void MainWindow::on_c_easingin_toggled(bool checked)
+{
+    if (editingKeyframe == NULL) return;
+    if (checked) {
+        ui->e_easingin->setEnabled(true);
+        ui->e_easingin->setCurrentIndex(0);
+        editingKeyframe->easeIn = Keyframe::LinearEasing;
+    } else {
+        ui->e_easingin->setCurrentIndex(0);
+        ui->e_easingin->setEnabled(false);
+        editingKeyframe->easeIn = Keyframe::NoEasing;
+    }
+}
+
+void MainWindow::on_c_easingout_toggled(bool checked)
+{
+    if (editingKeyframe == NULL) return;
+    if (checked) {
+        ui->e_easingout->setEnabled(true);
+        ui->e_easingin->setCurrentIndex(0);
+        editingKeyframe->easeOut = Keyframe::LinearEasing;
+        qDebug() <<editingKeyframe->easeOut;
+    } else {
+        ui->e_easingout->setCurrentIndex(0);
+        ui->e_easingout->setEnabled(false);
+        editingKeyframe->easeOut = Keyframe::NoEasing;
+    }
+}
+
+void MainWindow::on_e_easingout_currentIndexChanged(int index)
+{
+    if (editingKeyframe == NULL) return;
+    if (ui->c_easingout->isEnabled()) {
+        if (index == 0) editingKeyframe->easeOut = Keyframe::LinearEasing;
+        if (index == 1) editingKeyframe->easeOut = Keyframe::CubicEasing;
+        if (index == 2) editingKeyframe->easeOut = Keyframe::QuadraticEasing;
+        if (index == 3) editingKeyframe->easeOut = Keyframe::SinusoidalEasing;
+        if (index == 4) editingKeyframe->easeOut = Keyframe::ExponentialEasing;
+    }
+}
+
+void MainWindow::on_e_easingin_currentIndexChanged(int index)
+{
+    if (editingKeyframe == NULL) return;
+    if (ui->c_easingin->isEnabled()) {
+        if (index == 0) editingKeyframe->easeIn = Keyframe::LinearEasing;
+        if (index == 1) editingKeyframe->easeIn = Keyframe::CubicEasing;
+        if (index == 2) editingKeyframe->easeIn = Keyframe::QuadraticEasing;
+        if (index == 3) editingKeyframe->easeIn = Keyframe::SinusoidalEasing;
+        if (index == 4) editingKeyframe->easeIn = Keyframe::ExponentialEasing;
+    }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -27,9 +27,17 @@ private slots:
     void on_e_timelineScroll_sliderMoved(int position);
     void on_e_timelineScroll_valueChanged(int value);
     void onTimelinePositionChanged(int x);
-    void onKeyframeSelected(KeyframeObject* obj);
+    void onKeyframeSelected(KeyframeObject* obj, bool isint);
+    void on_e_value_textEdited(const QString &arg1);
+    void on_e_value_editingFinished();
+    void on_c_easingin_toggled(bool checked);
+    void on_c_easingout_toggled(bool checked);
+    void on_e_easingout_currentIndexChanged(int index);
+    void on_e_easingin_currentIndexChanged(int index);
+
 private:
     KeyframeObject *editingKeyframe = NULL;
+    bool isInt = true;
     Ui::MainWindow *ui;
 };
 
